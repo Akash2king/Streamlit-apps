144639cd32cbad5edfb22f0dfdf13ebc91f00837
Logesh <89137121+Akash2king@users.noreply.github.com>
2024-05-05 14:24:41 +0530

    Update text_summarizer.py
    split to columns

--- text_summarizer.py
+++ text_summarizer.py
@@ -6,23 +6,24 @@ from urllib.request import urlopen
 API_URL = "https://api-inference.huggingface.co/models/pszemraj/led-large-book-summary"
 key=st.secrets["auth_key"]
 headers = {"Authorization": f"Bearer {key}"}
-
-st.title("Doc Summarizer Extracting Key Insights from Texts 🔗")
-st.markdown('''
-## Team Members:
-- Logeshwaran V (621321205032)
-- Sanjay B (621321205042)
-- Kanish P A (621321205027)
-
-## Guide:
-- Mr. J. Sathishkumar, AP / IT
-
----
-
-**DocSummarizer** is an innovative project aimed at extracting key insights from texts efficiently. Led by a dedicated team of Logeshwaran V, Sanjay B, and Kanish P A, under the expert guidance of Mr. J. Sathishkumar, AP / IT, this project seeks to revolutionize the way we consume and analyze textual information.
-
-Stay tuned for updates on our progress and insights into the exciting world of text summarization!
-''')
+col1, col2= st.columns(2)
+with col1:
+    st.title("Doc Summarizer Extracting Key Insights from Texts 🔗")
+    st.markdown('''
+    ## Team Members:
+    - Logeshwaran V (621321205032)
+    - Sanjay B (621321205042)
+    - Kanish P A (621321205027)
+    
+    ## Guide:
+    - Mr. J. Sathishkumar, AP / IT
+    
+    ---
+    
+    **DocSummarizer** is an innovative project aimed at extracting key insights from texts efficiently. Led by a dedicated team of Logeshwaran V, Sanjay B, and Kanish P A, under the expert guidance of Mr. J. Sathishkumar, AP / IT, this project seeks to revolutionize the way we consume and analyze textual information.
+    
+    Stay tuned for updates on our progress and insights into the exciting world of text summarization!
+    ''')
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
@@ -48,29 +49,29 @@ def get_txt_from_url(urlinput):
     text = '\n'.join(chunk for chunk in chunks if chunk)
 
     return text
-
-input_option = st.selectbox("Select input option", ["Text Area",  "Upload Text File"])
-
-if input_option == "Text Area":
-    input_text = st.text_area("Enter text to summarize")
-    if st.button("Summarize"):
-        output = query({"inputs": input_text})
-       
-        st.write(output[0]["summary_text"])
-
-# elif input_option == "URL":
-#     input_url = st.text_input("Enter URL")
-#     if st.button("Summarize"):
-       
-#         content=get_txt_from_url(input_url)
-#         output = query({"inputs": content})
-#         st.write(output[0]["summary_text"])
-        
-
-elif input_option == "Upload Text File":
-    uploaded_file = st.file_uploader("Choose a text file", type=["txt"])
-    if uploaded_file is not None:
-        file_contents = uploaded_file.read().decode("utf-8")
+with col2:
+    input_option = st.selectbox("Select input option", ["Text Area",  "Upload Text File"])
+    
+    if input_option == "Text Area":
+        input_text = st.text_area("Enter text to summarize")
         if st.button("Summarize"):
-            output = query({"inputs": file_contents})
+            output = query({"inputs": input_text})
+           
             st.write(output[0]["summary_text"])
+    
+    # elif input_option == "URL":
+    #     input_url = st.text_input("Enter URL")
+    #     if st.button("Summarize"):
+           
+    #         content=get_txt_from_url(input_url)
+    #         output = query({"inputs": content})
+    #         st.write(output[0]["summary_text"])
+            
+    
+    elif input_option == "Upload Text File":
+        uploaded_file = st.file_uploader("Choose a text file", type=["txt"])
+        if uploaded_file is not None:
+            file_contents = uploaded_file.read().decode("utf-8")
+            if st.button("Summarize"):
+                output = query({"inputs": file_contents})
+                st.write(output[0]["summary_text"])
